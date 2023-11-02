d07e479fa228050c607cc200a2b7c81ce3e43fdd
xRa1ny <mikeneumann2302@gmail.com>
2023-11-02 14:58:37 +0100

    #10: migrated current hologram config system to Vital config system

--- vital-holograms/src/main/java/me/xra1ny/vital/holograms/VitalHologramManager.java
+++ vital-holograms/src/main/java/me/xra1ny/vital/holograms/VitalHologramManager.java
@@ -4,15 +4,10 @@ import lombok.Getter;
 import lombok.SneakyThrows;
 import lombok.extern.java.Log;
 import me.xra1ny.vital.core.VitalComponentListManager;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,26 +23,16 @@ public final class VitalHologramManager extends VitalComponentListManager<VitalH
 
     private final JavaPlugin javaPlugin;
 
-    /**
-     * The config file of this hologram manager.
-     */
-    private final File configFile;
-
-    /**
-     * The config of this hologram manager.
-     */
-    private final FileConfiguration config;
+    private final VitalHologramConfig vitalHologramConfig;
 
     /**
      * Constructs a new VitalHologramManagement instance.
      *
      * @param javaPlugin The JavaPlugin instance associated with this manager.
      */
-    public VitalHologramManager(@NotNull JavaPlugin javaPlugin) {
+    public VitalHologramManager(@NotNull JavaPlugin javaPlugin, @NotNull VitalHologramConfig vitalHologramConfig) {
         this.javaPlugin = javaPlugin;
-        this.configFile = new File(javaPlugin.getDataFolder(), "holograms.yml");
-        javaPlugin.saveResource(this.configFile.getName(), false);
-        this.config = YamlConfiguration.loadConfiguration(this.configFile);
+        this.vitalHologramConfig = vitalHologramConfig;
     }
 
     /**
@@ -57,13 +42,14 @@ public final class VitalHologramManager extends VitalComponentListManager<VitalH
      */
     @SneakyThrows
     public void saveVitalHologramToConfig(@NotNull VitalHologram vitalHologram) {
-        final ConfigurationSection section = this.config.createSection(vitalHologram.getName());
+        List<VitalHologram> vitalHologramList = vitalHologramConfig.getVitalHologramList();
 
-        section.set("lines", vitalHologram.getLines());
-        section.set("material", String.valueOf(vitalHologram.getDisplayType()));
-        section.set("location", vitalHologram.getLocation());
+        if(vitalHologramList == null) {
+            vitalHologramList = new ArrayList<>();
+        }
 
-        this.config.save(this.configFile);
+        vitalHologramList.add(vitalHologram);
+        vitalHologramConfig.setVitalHologramList(vitalHologramList);
     }
 
     /**
@@ -73,38 +59,26 @@ public final class VitalHologramManager extends VitalComponentListManager<VitalH
      */
     @SneakyThrows
     public void removeVitalHologramFromConfig(@NotNull VitalHologram vitalHologram) {
-        this.config.set(vitalHologram.getName(), null);
-        this.config.save(this.configFile);
+        final List<VitalHologram> vitalHologramList = vitalHologramConfig.getVitalHologramList();
+
+        if(vitalHologramList == null || vitalHologramList.isEmpty()) {
+            return;
+        }
+
+        vitalHologramList.remove(vitalHologram);
+        vitalHologramConfig.setVitalHologramList(vitalHologramList);
     }
 
     /**
      * Called when this VitalHologramManagement instance is registered.
      * Loads hologram data from the configuration file and creates VitalHologram instances.
      */
-    @SuppressWarnings("unchecked")
     @Override
     public void onRegistered() {
-        for (String key : this.config.getKeys(false)) {
-            final ConfigurationSection section = this.config.getConfigurationSection(key);
-
-            if (section == null) {
-                continue;
+        if(vitalHologramConfig.getVitalHologramList() != null) {
+            for(VitalHologram vitalHologram : vitalHologramConfig.getVitalHologramList()) {
+                registerVitalComponent(vitalHologram);
             }
-
-            final List<String> lines = (List<String>) section.getList("lines");
-            final String materialName = section.getString("material");
-            final Material material = materialName == null ? null : Material.valueOf(materialName);
-            final Location location = section.getLocation("location");
-
-            // Skip hologram creation if values are null
-            if (lines == null || location == null) {
-                continue;
-            }
-
-            // Attempt to create the loaded hologram from config information...
-            final VitalHologram vitalHologram = new VitalHologram(key, location, material, lines.toArray(new String[0]));
-
-            registerVitalComponent(vitalHologram);
         }
 
         log.info("Successfully registered VitalHologramManagement!");
