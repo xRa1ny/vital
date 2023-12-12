0b92e2ea2992841088c46dc3bb77cc719eb4a397
Mike Neumann <mikeneumann2302@gmail.com>
2023-12-12 16:00:12 +0100

    #63: implemented methods for sound and message broadcasting in `VitalCore`

--- vital-core/src/main/java/me/xra1ny/vital/core/VitalCore.java
+++ vital-core/src/main/java/me/xra1ny/vital/core/VitalCore.java
@@ -2,6 +2,9 @@ package me.xra1ny.vital.core;
 
 import lombok.Getter;
 import lombok.extern.java.Log;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.reflections.Reflections;
@@ -9,6 +12,7 @@ import org.reflections.Reflections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 /**
  * The main instance of the Vital framework.
@@ -86,9 +90,7 @@ public abstract class VitalCore<T extends JavaPlugin> {
         // scan for all classes annotated with `@VitalAutoRegistered` and attempt to register them on the base manager.
         // NOTE: this configuration is entirely user dependent, if a user wrongly implements their auto registered components,
         // with components that cannot be dependency injected, this registration will not work!!!
-        for (Class<?> vitalComponentClass : new Reflections().getTypesAnnotatedWith(VitalAutoRegistered.class).stream()
-                .filter(VitalComponent.class::isAssignableFrom)
-                .toList()) {
+        for (Class<?> vitalComponentClass : new Reflections().getTypesAnnotatedWith(VitalAutoRegistered.class).stream().filter(VitalComponent.class::isAssignableFrom).toList()) {
             // assume every class is extended from `VitalComponent` since we filtered in our chain above.
             final Optional<? extends VitalComponent> optionalVitalComponent = (Optional<? extends VitalComponent>) DIUtils.getDependencyInjectedInstance(vitalComponentClass);
 
@@ -114,4 +116,84 @@ public abstract class VitalCore<T extends JavaPlugin> {
      * Called when this VitalCore is enabled
      */
     public abstract void onEnable();
+
+    /**
+     * Broadcasts a message to all players currently connected to the server.
+     * Identical to {@link Bukkit#broadcastMessage(String)}.
+     * This method is supplied for convenience.
+     *
+     * @param message The message to broadcast.
+     */
+    public static void broadcast(@NotNull String message) {
+        Bukkit.broadcastMessage(message);
+    }
+
+    /**
+     * Broadcasts a message to all players currently connected to the server, matching the given {@link Predicate}.
+     *
+     * @param message         The message to broadcast.
+     * @param playerPredicate The Predicate specifying the condition in which the message should be broadcast.
+     */
+    public static void broadcast(@NotNull String message, @NotNull Predicate<Player> playerPredicate) {
+        for (Player player : Bukkit.getOnlinePlayers().stream()
+                .filter(playerPredicate)
+                .toList()) {
+            player.sendMessage(message);
+        }
+    }
+
+    /**
+     * Broadcasts a {@link Sound} to all players currently connected to this server.
+     * volume: 1f, pitch: 1f.
+     *
+     * @param sound The sound to broadcast.
+     */
+    public static void broadcast(@NotNull Sound sound) {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.playSound(player, sound, 1f, 1f);
+        }
+    }
+
+    /**
+     * Broadcasts a {@link Sound} to all players currently connected to this server, matching the given {@link Predicate}.
+     *
+     * @param sound           The sound to broadcast.
+     * @param playerPredicate The Predicate specifying the condition in which the sound should be broadcast.
+     */
+    public static void broadcast(@NotNull Sound sound, @NotNull Predicate<Player> playerPredicate) {
+        for (Player player : Bukkit.getOnlinePlayers().stream()
+                .filter(playerPredicate)
+                .toList()) {
+            player.playSound(player, sound, 1f, 1f);
+        }
+    }
+
+    /**
+     * Broadcasts a {@link Sound} to all players currently connected to this server.
+     *
+     * @param sound  The sound to broadcast.
+     * @param volume The volume of the sound.
+     * @param pitch  The pitch of the sound.
+     */
+    public static void broadcast(@NotNull Sound sound, float volume, float pitch) {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.playSound(player, sound, volume, pitch);
+        }
+    }
+
+    /**
+     * Broadcasts a {@link Sound} to all players currently connected to this server, matching the given {@link Predicate}.
+     *
+     * @param sound           The sound to broadcast.
+     * @param playerPredicate The Predicate specifying the condition in which the sound is broadcast.
+     * @param volume          The volume of the sound.
+     * @param pitch           The pitch of the sound.
+     */
+    public static void broadcast(@NotNull Sound sound, @NotNull Predicate<Player> playerPredicate, float volume, float pitch) {
+        for (Player player : Bukkit.getOnlinePlayers().stream()
+                .filter(playerPredicate)
+                .toList()) {
+            player.playSound(player, sound, volume, pitch);
+        }
+    }
 }
